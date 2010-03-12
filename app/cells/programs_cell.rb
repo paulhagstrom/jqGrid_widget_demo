484e33e479e41dd64ebb4199bfc6ea4a18e0875a
Paul Hagstrom <hagstrom@That-One-MacBook.local>
2010-03-12 13:15:54 -0500

    Replaced a corrupt jquery-ui, and a couple of documentation notes.

--- app/cells/programs_cell.rb
+++ app/cells/programs_cell.rb
@@ -2,7 +2,7 @@ class ProgramsCell < JqgridWidgetCell
   
   def _setup
     super do |col|
-      col.add_column('name', :width => 100, :sortable => 'default')
+      col.add_column('name', :width => 250, :sortable => 'default')
     end
     @jqgrid_options.update({
       :height => 50
