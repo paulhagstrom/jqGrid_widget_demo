d08b011cc7bb097ee856a5d8bf856bc6c5c5f364
Paul Hagstrom <hagstrom@That-One-MacBook.local>
2010-03-12 14:37:35 -0500

    Little updates with links and layouts

--- app/cells/degrees_cell.rb
+++ app/cells/degrees_cell.rb
@@ -7,9 +7,9 @@ class DegreesCell < JqgridWidgetCell
       col.add_column('program_id', :width => 50, :sortable => false, :custom => :program_name)
     end
     @jqgrid_options.update({
+      :rows_per_page => 20,
       :height => 350
     })
-    @rows_per_page = 20
     render
   end
 
