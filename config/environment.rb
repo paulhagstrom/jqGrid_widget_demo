5701e5287596fa0507d0df0f9e4b2a3856341e53
Paul Hagstrom <hagstrom@That-One-MacBook.local>
2010-08-21 14:06:24 -0400

    Removed cells submodule (in favor of gem dependency)

--- config/environment.rb
+++ config/environment.rb
@@ -2,12 +2,13 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 # RAILS_GEM_VERSION = '2.3.2' unless defined? RAILS_GEM_VERSION
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem "cells", :version => '3.3.4'
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
